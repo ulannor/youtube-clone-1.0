d62e6f7856867a879a23b88a7316cc614d8c9352
WintBree <u.duulatov@gmail.com>
2024-06-15 20:32:51 +0600

    Fetch data problem - baseline

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -15,7 +15,7 @@ const ChannelDetail = () => {
 
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
-      console.log(data)
+      setChannelDetail(data?.items[0])
     );
 
     fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) =>
