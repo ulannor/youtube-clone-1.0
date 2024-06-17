f4664d1cfa529b22c90143a379e2f054bc3e3453
WintBree <u.duulatov@gmail.com>
2024-06-18 00:43:44 +0600

    new features

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -16,9 +16,6 @@ const VideoDetail = () => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
     );
-    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
-      (data) => setVideos(data.items)
-    );
   }, [id]);
 
   if (!videoDetail?.snippet) return "Loading...";
