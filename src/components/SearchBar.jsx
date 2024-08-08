b2d8d922a46ee82a99e9e41c70b9891584bfda6e
WintBree <u.duulatov@gmail.com>
2024-08-08 16:53:31 +0600

    search bar done - part 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,17 +11,20 @@ const Search = styled("div")(({ theme }) => ({
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  // maxWidth: "80%",
-  // [theme.breakpoints.down("sm")]: {
-  //   marginLeft: theme.spacing(1),
-  //   width: "80%",
-  // },
+
+  [theme.breakpoints.down("sm")]: {
+    backgroundColor: alpha(theme.palette.common.white, 0),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.1),
+    },
+  },
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
   height: "100%",
   position: "absolute",
+  borderRight: "1px solid white",
   pointerEvents: "none",
   display: "flex",
   alignItems: "center",
@@ -30,18 +33,11 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
-  // width: "100%",
+  width: "100%",
   "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    // [theme.breakpoints.up("md")]: {
-    //   width: "40ch",
-    //   "&:focus": {
-    //     width: "100ch",
-    //   },
-    // },
   },
 }));
 
